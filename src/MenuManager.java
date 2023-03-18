f2322b7e60e2c0a3033f37ef434533f6f3f9d227
Oreon0529 <dhwnsgur4449@naver.com>
2023-03-18 21:14:33 +0900

    removed hello.java and added MenuManager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,12 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Checked-out Books");
+		System.out.println("2. Returned Books");
+		System.out.println("3. Edit Book Number");
+		System.out.println("4. View All Books");
+		System.out.println("5. Exit");
+	}
+
+}
