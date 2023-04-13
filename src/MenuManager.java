3135cd3badb59fb01a2e7037ed130eab19729105
Oreon0529 <dhwnsgur4449@naver.com>
2023-04-13 21:57:05 +0900

    4/13

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,59 +1,32 @@
-import java.util.Scanner; 
+import java.util.Scanner;
+
 public class MenuManager {
-	public static void main(String[] args) {
-		int num = 5;
-		Scanner input = new Scanner(System.in);
-		while (num != 6) {
-			System.out.println("1. Add Books");
-			System.out.println("2. Delete Books");
-			System.out.println("3. Edit Books");
-			System.out.println("4. View Books");
-			System.out.println("5. Show a Menu");
-			System.out.println("6. Exit");
-			System.out.println("Select one number between 1-6: "); 
-			num = input.nextInt();
-			
-			if (num == 1) {
-				addStudent();
-			} else if (num == 2) {
-				deleteStudent(); 
-			} else if (num == 3) {
-				editStudent();
-			} else if (num == 4) {
-				viewStudent(); 
-			} else {
-				continue; 
-			}
-		}
-	}
-	public static void addStudent() {
-		Scanner input = new Scanner(System.in); 
-		System.out.print("Student ID: ");
-		int studentId = input.nextInt(); 
-		System.out.print("Student name: "); 
-		String studentName = input.next(); 
-		System.out.print("Email address: "); 
-		String studentEmail = input.next(); 
-		System.out.println(studentEmail); 
-		System.out.print("Phone number: "); 
-		String studentPhone = input.next();
-	}
-	
-	public static void deleteStudent() { 
-		Scanner input = new Scanner(System.in); 
-		System.out.print("Student ID: ");
-		int studentId = input.nextInt(); 
-	}
-	
-	public static void editStudent() {
-		Scanner input = new Scanner(System.in); 
-		System.out.print("Student ID: ");
-		int studentId = input.nextInt(); 
-	}
-	
-	public static void viewStudent() {
-		Scanner input = new Scanner(System.in); 
-		System.out.print("Student ID: ");
-		int studentId = input.nextInt(); 
-	}
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    	BooksManager booksManager = new BooksManager(input);
+
+        int num = -1;
+        while (num != 5) {
+            System.out.println("*** Books Management System Menu ***");
+            System.out.println("1. Checked-out Books");
+            System.out.println("2. Returned Books");
+            System.out.println("3. Edit Book Info");
+            System.out.println("4. View All Books");
+            System.out.println("5. Exit");
+            System.out.println("Select one number between 1-5: "); 
+            num = input.nextInt();
+
+            if (num == 1) {
+            	booksManager.checkedOut();
+            } else if (num == 2) {
+            	booksManager.returned();    
+            } else if (num == 3) {
+            	booksManager.editBooks();    
+            } else if (num == 4) {
+            	booksManager.viewBooks();
+            } else {
+                continue;
+            }
+        }
+    }
 }
