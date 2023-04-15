487b7524cf0fa8f622e74f40a527527a3aa0a198
Oreon0529 <dhwnsgur4449@naver.com>
2023-04-15 15:13:25 +0900

    4/15

--- src/Book.java
+++ src/Book.java
@@ -1,24 +1,23 @@
 public class Book {
-    String title;
-    int id;
-    String date;
-    
-    public Book() {
-    }
-    
-    public Book(String title, int id) {
-        this.title = title;
-        this.id = id;
-    }
-    
-    public Book(String title, int id, String date) {
-        this.title = title;
-        this.id = id;
-        this.date = date;
-    }
+	String title;
+	int id;
+	String date;
 
+	public Book() {
+	}
 
-    public void printInfo() {
-    	System.out.println(" title: " + title + " id: " + id + " date: " + date);
-    }
+	public Book(String title, int id) {
+		this.title = title;
+		this.id = id;
+	}
+
+	public Book(String title, int id, String date) {
+		this.title = title;
+		this.id = id;
+		this.date = date;
+	}
+
+	public void printInfo() {
+		System.out.println(" title: " + title + " id: " + id + " date: " + date);
+	}
 }
